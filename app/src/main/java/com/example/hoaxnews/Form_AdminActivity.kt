b7d575c00f31f801b7b2e5c5c804da481fc8ede4
ooka <ookapratama2@gmail.com>
2022-04-10 23:39:48 +0800

    add form admin activity

---
+++ app/src/main/java/com/example/hoaxnews/Form_AdminActivity.kt
@@ -0,0 +1,11 @@
+package com.example.hoaxnews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Form_AdminActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_admin)
+    }
+}
