1bc14360bd811478b28d8302165d71e4e3268c86
murah ji <imurahji@gmail.com>
2022-04-15 07:24:42 +0800

    add logic, firebase on login/register (blum selesai)

--- app/src/main/java/com/example/hoaxnews/Form_RegisterActivity.kt
+++ app/src/main/java/com/example/hoaxnews/Form_RegisterActivity.kt
@@ -1,11 +1,68 @@
 package com.example.hoaxnews
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class Form_RegisterActivity : AppCompatActivity() {
+
+    lateinit var namas : EditText;
+    lateinit var pass : EditText;
+    lateinit var RedirectLogin : TextView;
+    private lateinit var regist : Button;
+
+    // firebase autentikasi
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_register)
+
+        // Find id xml
+        namas = findViewById(R.id.username)
+        pass = findViewById(R.id.password)
+        regist = findViewById(R.id.login)
+        RedirectLogin = findViewById(R.id.redirect)
+
+        auth = Firebase.auth
+
+        regist.setOnClickListener {
+            signUpUser()
+        }
+
+        RedirectLogin.setOnClickListener {
+            val intent = Intent(this, Form_UserActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+    private fun signUpUser() {
+        val nama = namas.text.toString()
+        val pass = pass.text.toString()
+
+        // check pass
+        if (nama.isBlank() || pass.isBlank()) {
+            Toast.makeText(this, "Nama dan Password tidak boleh kosong", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        // jika nama dan pass benar
+        auth.createUserWithEmailAndPassword(nama, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Berhasil Register", Toast.LENGTH_SHORT).show()
+                finish()
+            } else {
+                Toast.makeText(this, "Gagal Register", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/hoaxnews/Form_UserActivity.kt
+++ app/src/main/java/com/example/hoaxnews/Form_UserActivity.kt
@@ -1,11 +1,68 @@
 package com.example.hoaxnews
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.example.hoaxnews.databinding.ActivityFormUserBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class Form_UserActivity : AppCompatActivity() {
+    private lateinit var daftar :  TextView
+    lateinit var namas : EditText
+    private lateinit var pass : EditText
+    lateinit var btnLogin : Button
+    lateinit var redirectRegist : TextView
+
+    private lateinit var binding : ActivityFormUserBinding
+    private lateinit var actionBar : ActionBar
+    private lateinit var progresDialog : ProgressDialog
+
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_form_user)
+
+        daftar = findViewById(R.id.daftar)
+        namas = findViewById(R.id.username)
+        pass = findViewById(R.id.password)
+        btnLogin = findViewById(R.id.login)
+
+
+        auth = FirebaseAuth.getInstance()
+
+        btnLogin.setOnClickListener {
+            logins()
+        }
+
+        daftar.setOnClickListener {
+            val intent = Intent(this, Form_RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+    private fun logins() {
+        val nama = namas.text.toString().trim()
+        val pass = pass.text.toString()
+
+        auth.signInWithEmailAndPassword(nama, pass).addOnCompleteListener(this) {
+            if (it.isSuccessful) {
+                Toast.makeText(this, "Berhasil Login", Toast.LENGTH_SHORT).show()
+            }  else {
+                Toast.makeText(this, "Gagal Login", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+
+
     }
 }
