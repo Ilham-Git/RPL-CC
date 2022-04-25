2e3e6ff170e1497ce9af23655b466dc2cd3e2fe6
ooka <ookapratama2@gmail.com>
2022-04-25 23:15:22 +0800

    add edukasiberita (progress)

--- app/src/main/java/com/example/hoaxnews/EdukasiActivity.kt
+++ app/src/main/java/com/example/hoaxnews/EdukasiActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class EdukasiActivity : AppCompatActivity() {
 
-
+    
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
